f6e0f268faea05bba8dfea1c59e20ff2b70dd891
Dipak Ugale <ugale.deepak3010@gmail.com>
2025-02-14 14:29:45 +0530

    Initial add

---
+++ TDD_SpringBoot/src/main/java/com/TaskManager/TddSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.TaskManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddSpringBootApplication.class, args);
+	}
+
+}
